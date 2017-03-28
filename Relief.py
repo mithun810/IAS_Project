f6f98c97a9367433c91d4803f8ed010f9344145c
parasitecruncher <prashanth8314@gmail.com>
2017-03-28 18:27:29 +0530

    Fixed Relief; Added Merged dataset; Added ReliefRanks

--- Relief.py
+++ Relief.py
@@ -1,44 +1,77 @@
+'''
+IMPORTANT
+Instructions to run this program
+Use this program as module calling load_and_run(filename,number_of_iterations,BenignClassValue)
+returns a rank array'''
 import pandas as pd
 import numpy as np
 import random
 import math
-def relief(df,m,label):
-    f_weight=np.zeros(shape=df.shape[0])
-    df_wlabel=get_dataframe_with_label(df,label)
-    df_wolabel=get_dataframe_without_label(df,label)
-    
+import os
+
+
+
+def relief(df, m, label):
+    f_weight = np.zeros(shape=df.shape[1]-1)
+    df_wlabel = get_dataframe_with_label(df, label)
+    df_wolabel = get_dataframe_without_label(df, label)
     for i in f_weight:
-        i=0
-    for i in range(df.shape[1]):
+        i = 0
+    time=0
+    for featurenumber in range(df.shape[1]-1):
         for j in range(m):
-            randomindex=random.randint(0,df.shape[0])
-            f=df.iloc[randomindex,i]
-            f_weight[i]=f_weight[i]-(math.pow((f-nearest(df_wlabel,f,i)),2)+math.pow(f-nearest(df_wolabel,f,i)))
-        f_weight[i]=f_weight[i]/m
+            os.system('clear')
+            time=time+1
+            print ((1.0*time)/((df.shape[1]-1)*m))*100.0,"%"
+            randomindex = random.randint(0, df.shape[0]-1)
+            featurevector = df.iloc[randomindex, i]  # fixed feature random index
+            f_weight[featurenumber] = f_weight[featurenumber] - (math.pow(featurevector - nearest(df_wlabel, featurevector, featurenumber), 2)
+                                         + math.pow(featurevector - nearest(df_wolabel, featurevector, featurenumber),2))
+        f_weight[featurenumber] = f_weight[featurenumber] / m
+    print f_weight
+    array = np.array(f_weight)
+    temp = array.argsort()
+    ranks = np.empty(len(array), int)
+    ranks[temp] = np.arange(len(array))
+    print ranks
+    np.savetxt("ReliefRanks.csv", ranks, delimiter=",")
+    return ranks
 
-    return f_weight
 
-def get_dataframe_with_label(df,label):
-    n_df=pd.DataFrame(columns=df.columns)
-    row=0
+def get_dataframe_with_label(df, label):
+    n_df = pd.DataFrame(columns=df.columns)
+    row = 0
     for i in range(df.shape[0]):
-        if(df.loc[i]['ClassLabel']==label):
-            a.loc[row]=df.loc[i]
-            row=row+1
+        if (df.loc[i]['Class'] == label):
+            n_df.loc[row] = df.loc[i]
+            row = row + 1
     return n_df
 
-def get_dataframe_without_label(df,label):
-    n_df=pd.DataFrame(columns=df.columns)
-    row=0
 
+def get_dataframe_without_label(df, label):
+    n_df = pd.DataFrame(columns=df.columns)
+    row = 0
     for i in range(df.shape[0]):
-        if(df.loc[i]['ClassLabel']!=label):
-            n_df.loc[row]=df.loc[i]
-            row=row+1
+        if (df.loc[i]['Class'] != label):
+            n_df.loc[row] = df.loc[i]
+            row = row + 1
     return n_df
-def nearest(df,x,feature):
-    smallest=0
+
+
+def nearest(df, featurevector, featurenumber):
+    smallest = 0
+    smallestvalue = df.iloc[0][featurenumber]
     for i in range(df.shape[0]):
-        if(df.iloc[i,x]<df.iloc[smallest,x]):
+        if abs(df.iloc[i][featurenumber] - featurevector) < smallestvalue:
+            smallestvalue = df.iloc[i][featurenumber] - df.iloc[smallest][featurenumber]
             smallest = i
-    return df.iloc[smallest,x]
+    return df.iloc[smallest][featurenumber]
+
+def load_and_run(filename,m,BenignClassValue):
+    return relief(pd.read_csv(filename),m,BenignClassValue)
+def main():
+    x=relief(pd.read_csv("merged.csv"), 100, "None")
+    return x
+
+if __name__ == '__main__':
+    main()
